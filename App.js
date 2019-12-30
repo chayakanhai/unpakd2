a972598fc5b25d296560ff90fa6a73a777bc23d0
ZF <zinat.farhang@gmail.com>
2019-12-30 15:39:53 +0100

    getting modules, successful run

--- App.js
+++ App.js
@@ -1,63 +1,114 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, FlatList, Button, Image } from "react-native";
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
 
-import IngredientItem from "./components/IngredientItem";
-import IngredientInput from "./components/IngredientInput";
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
 
-export default function App() {
-  const [recipeIngredients, setRecipeIngredients] = useState([]);
-  const [isAddMode, setIsAddMode] = useState(false);
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
-  const addIngredientHandler = ingredientTitle => {
-    setRecipeIngredients(currentIngredients => [
-      ...currentIngredients,
-      { id: Math.random().toString(), value: ingredientTitle }
-    ]);
-    setIsAddMode(false);
-  };
-
-  const removeGoalHandler = ingredientId => {
-    setRecipeIngredients(currentIngredients => {
-      return currentIngredients.filter(
-        ingredient => ingredient.id !== ingredientId
-      );
-    });
-  };
-
+const App: () => React$Node = () => {
   return (
-    <View style={styles.screen}>
-      <Image style={styles.image} source={require("./assets/vegs.jpg")}></Image>
-      <Button title="Add New Ingredient" onPress={() => setIsAddMode(true)} />
-      <IngredientInput
-        visible={isAddMode}
-        onAddIngredient={addIngredientHandler}
-      />
-      <FlatList
-        keyExtractor={(item, index) => item.id}
-        data={recipeIngredients}
-        renderItem={itemData => (
-          <IngredientItem
-            id={itemData.item.id}
-            onDelete={removeGoalHandler}
-            title={itemData.item.value}
-          />
-        )}
-      />
-    </View>
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
+          )}
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Step One</Text>
+              <Text style={styles.sectionDescription}>
+                Edit <Text style={styles.highlight}>App.js</Text> to change this
+                screen and then come back to see your edits.
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>See Your Changes</Text>
+              <Text style={styles.sectionDescription}>
+                <ReloadInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Debug</Text>
+              <Text style={styles.sectionDescription}>
+                <DebugInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Learn More</Text>
+              <Text style={styles.sectionDescription}>
+                Read the docs to discover what to do next:
+              </Text>
+            </View>
+            <LearnMoreLinks />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
   );
-}
+};
+
 const styles = StyleSheet.create({
-  screen: {
-    padding: 50,
-    backgroundColor: "black",
-    position: "absolute",
-    width: "100%",
-    height: "100%",
-    justifyContent: "center"
+  scrollView: {
+    backgroundColor: Colors.lighter,
   },
-  image: {
-    width: "100%",
-    height: "50%",
-    justifyContent: "flex-start"
-  }
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
 });
+
+export default App;
